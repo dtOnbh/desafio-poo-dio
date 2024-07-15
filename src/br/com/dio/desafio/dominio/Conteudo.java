c0a0e0f5a1d37dcd26d9d8c9253d6fcfb2609299
dtOnbh <daltonsilva.felix@gmail.com>
2024-07-15 16:57:53 -0300

    Update Conteudo.java

--- src/br/com/dio/desafio/dominio/Conteudo.java
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -1,14 +1,13 @@
 package br.com.dio.desafio.dominio;
 
 public abstract class Conteudo {
-
     protected static final double XP_PADRAO = 10d;
-
     private String titulo;
     private String descricao;
 
     public abstract double calcularXp();
 
+    // Getters e Setters
     public String getTitulo() {
         return titulo;
     }
